e4bc266cf3e953d6b81955382fdf08323d46d52a
Joe <joeboy229@gmail.com>
2022-02-07 16:22:03 +0800

    separate smaller components to make them reusable

--- android-compose/BasicsCodelab/app/src/main/java/com/joe/basicscodelab/MainActivity.kt
+++ android-compose/BasicsCodelab/app/src/main/java/com/joe/basicscodelab/MainActivity.kt
@@ -18,12 +18,17 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             BasicsCodelabTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+                MyApp()
             }
         }
+    }
+}
+
+@Composable
+private fun MyApp() {
+    // A surface container using the 'background' color from the theme
+    Surface(color = MaterialTheme.colors.background) {
+        Greeting("Android")
     }
 }
 
@@ -38,6 +43,6 @@ fun Greeting(name: String) {
 @Composable
 fun DefaultPreview() {
     BasicsCodelabTheme {
-        Greeting("Android")
+        MyApp()
     }
 }

55c6f0d3f72a682c30b2b74283a94120b2fa9308
Joe <joeboy229@gmail.com>
2022-02-07 17:44:50 +0800

    State hoisting - switch two different views.

--- android-compose/BasicsCodelab/app/src/main/java/com/joe/basicscodelab/MainActivity.kt
+++ android-compose/BasicsCodelab/app/src/main/java/com/joe/basicscodelab/MainActivity.kt
@@ -3,17 +3,10 @@ package com.joe.basicscodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedButton
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -31,7 +24,58 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MyApp(names: List<String> = listOf("World", "Compose")) {
+fun MyApp() {
+
+    // add the logic to show the different screens in MyApp, and hoist the state.
+    // shouldShowOnboarding is using a by keyword instead of the =. This is a property delegate that saves you from typing .value every time.
+    var shouldShowOnboarding by remember { mutableStateOf(true) }
+
+    // In Compose you don't hide UI elements. Instead, you simply don't add them to the composition,
+    // so they're not added to the UI tree that Compose generates.
+    // You do this with simple conditional Kotlin logic.
+    // For example to show the onboarding screen or the list of greetings you would do something like:
+    if (shouldShowOnboarding) {
+        OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
+    } else {
+        Greetings()
+    }
+}
+
+@Composable
+fun OnboardingScreen(onContinueClicked: () -> Unit) {
+
+    Surface {
+        // Column can be configured to display its contents in the center of the screen.
+        // The modifier align can be used to align composables inside a row or a column.
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text("Welcome to the Basics Codelab!")
+            Button(
+                modifier = Modifier.padding(vertical = 24.dp),
+                onClick = onContinueClicked
+            ) {
+                Text("Continue")
+            }
+        }
+    }
+}
+
+// You have added a new composable called OnboardingScreen and also a new preview.
+// If you build the project you'll notice you can have multiple previews at the same time.
+// We also added a fixed height to verify that the content is aligned correctly.
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnboardingPreview() {
+    BasicsCodelabTheme {
+        OnboardingScreen(onContinueClicked = {})
+    }
+}
+
+@Composable
+private fun Greetings(names: List<String> = listOf("World", "Compose")) {
     Column(modifier = Modifier.padding(vertical = 4.dp)) {
         for (name in names) {
             Greeting(name = name)
@@ -69,6 +113,6 @@ private fun Greeting(name: String) {
 @Composable
 fun DefaultPreview() {
     BasicsCodelabTheme {
-        MyApp()
+        Greetings()
     }
 }

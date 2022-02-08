1459feac9d34d1e3be550dd3b4100e7461741407
Joe <joeboy229@gmail.com>
2022-02-08 19:15:59 +0800

    Create a custom modifier and a custom layout

--- android-compose/LayoutsCodelab/app/src/main/java/com/joe/layoutscodelab/MainActivity.kt
+++ android-compose/LayoutsCodelab/app/src/main/java/com/joe/layoutscodelab/MainActivity.kt
@@ -18,8 +18,13 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.AlignmentLine
+import androidx.compose.ui.layout.FirstBaseline
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.layout.layout
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberImagePainter
 import com.joe.layoutscodelab.ui.theme.LayoutsCodelabTheme
@@ -81,7 +86,13 @@ fun BodyContent(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(8.dp)) {
         Text(text = "Hi there!")
         Text(text = "Thanks for going through the Layouts codelab")
-        ScrollingList()
+//        ScrollingList()
+        MyOwnColumn(modifier.padding(8.dp)) {
+            Text("MyOwnColumn")
+            Text("places items")
+            Text("vertically.")
+            Text("We've done it by hand!")
+        }
     }
 }
 
@@ -187,3 +198,91 @@ fun PhotographerCardPreview() {
         PhotographerCard()
     }
 }
+
+@Preview
+@Composable
+fun TextWithPaddingToBaselinePreview() {
+    LayoutsCodelabTheme {
+        Text("Hi there!", Modifier.firstBaselineToTop(32.dp))
+    }
+}
+
+@Preview
+@Composable
+fun TextWithNormalPaddingPreview() {
+    LayoutsCodelabTheme {
+        Text("Hi there!", Modifier.padding(top = 32.dp))
+    }
+}
+
+fun Modifier.firstBaselineToTop(
+    firstBaselineToTop: Dp
+) = this.then(
+    layout { measurable, constraints ->
+        val placeable = measurable.measure(constraints)
+
+        // Check the composable has a first baseline
+        check(placeable[FirstBaseline] != AlignmentLine.Unspecified)
+        val firstBaseline = placeable[FirstBaseline]
+
+        // Height of the composable with padding - first baseline
+        val placeableY = firstBaselineToTop.roundToPx() - firstBaseline
+        val height = placeable.height + placeableY
+        layout(placeable.width, height) {
+            // Where the composable gets placed
+            placeable.placeRelative(0, placeableY)
+        }
+
+    }
+)
+
+@Composable
+fun MyOwnColumn(
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit
+) {
+    Layout(
+        modifier = modifier,
+        content = content
+    ) { measurables, constraints ->
+        // Don't constrain child views further, measure them with given constraints
+        // List of measured children
+        val placeables = measurables.map { measurable ->
+            // Measure each child
+            measurable.measure(constraints)
+        }
+
+        // Track the y co-ord we have placed children up to
+        var yPosition = 0
+
+        // Set the size of the layout as big as it can
+        layout(constraints.maxWidth, constraints.maxHeight) {
+            // Place children in the parent layout
+            placeables.forEach { placeable ->
+                // Position item on the screen
+                placeable.placeRelative(x = 0, y = yPosition)
+
+                // Record the y co-ord placed up to
+                yPosition += placeable.height
+            }
+        }
+    }
+}
+
+/* Using the Layout composable */
+// Instead of controlling how a single composable gets measured and laid out on the screen,
+// you might have the same necessity for a group of composables.
+// For that, you can use the Layout composable to manually control how to measure and position the layout's children.
+//@Composable
+//fun CustomLayout(
+//    modifier: Modifier = Modifier,
+//    // custom layout attributes
+//    content: @Composable () -> Unit
+//) {
+//    Layout(
+//        modifier = modifier,
+//        content = content
+//    ) { measurables, constraints ->
+//        // measure and position children given constraints logic here
+//    }
+//}

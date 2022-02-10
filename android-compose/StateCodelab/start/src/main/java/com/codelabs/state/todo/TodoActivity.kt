61603be6f067691342c62afc4dbd1b00b69f3db2
Joe <joeboy229@gmail.com>
2022-02-10 15:32:03 +0800

    add TodoActivityScreen on TodoActivity

--- android-compose/StateCodelab/start/src/main/java/com/codelabs/state/todo/TodoActivity.kt
+++ android-compose/StateCodelab/start/src/main/java/com/codelabs/state/todo/TodoActivity.kt
@@ -21,6 +21,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
 import com.codelabs.state.ui.StateCodelabTheme
 
 class TodoActivity : AppCompatActivity() {
@@ -32,9 +33,19 @@ class TodoActivity : AppCompatActivity() {
         setContent {
             StateCodelabTheme {
                 Surface {
-                    // TODO: build the screen in compose
+                    TodoActivityScreen(todoViewModel)
                 }
             }
         }
     }
+}
+
+@Composable
+private fun TodoActivityScreen(todoViewModel: TodoViewModel) {
+    val items = listOf<TodoItem>()
+    TodoScreen(
+        items = items,
+        onAddItem = { },
+        onRemoveItem = { }
+    )
 }
